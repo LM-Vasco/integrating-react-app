bab137b2ea76678d09a65961593a380185157d42
Vasco Martins <vasco.martins.drummer@gmail.com>
2023-11-14 10:58:47 +0000

    projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom"
+
+import Homepage from './pages/Homepage'
+import ProjectListPage from './pages/ProjectListPage'
+import Navbar from "./components/NavBar"
+
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
+      <div className="App">
+        {<Navbar />}
+
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/projects" element={<ProjectListPage />} />
+        </Routes>
       </div>
     </>
   )
